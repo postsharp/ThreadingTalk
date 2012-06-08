2b9bf8362c1bc3fb129231cd055a24017e3370af
Gael Fraiteur <gael@sharpcrafters.com>
2012-06-08 21:36:02 +0200

    Initial version of the ping-pong game (with actors).

---
+++ PingPongDemo/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PostSharp.Toolkit.Threading;
+
+namespace TestAsync
+{
+    class Player : Actor
+    {
+        string name;
+        private int counter;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public async Task<int> GetCounter()
+        {
+            return this.counter;
+        }
+
+        public async Task Ping(Player peer, int countdown)
+        {
+            Console.WriteLine("{0}.Ping({1}) from thread {2}", this.name, countdown, Thread.CurrentThread.ManagedThreadId);
+
+            if (countdown > 1)
+            {
+                await peer.Ping(this, countdown - 1);
+            }
+
+            this.counter++;
+        }
+
+        [ThreadSafe]
+        public override string ToString()
+        {
+            return this.name;
+        }
+
+    }
+}
---
+++ PingPongDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().Wait();
+        }
+
+        private static async Task AsyncMain()
+        {
+
+            Player ping = new Player("Sarkozy");
+            Player pong = new Player("Hollande");
+
+            Task pingTask = ping.Ping(pong, 4);
+            Task pongTask = pong.Ping(ping, 4);
+
+            await pingTask;
+            await pongTask;
+
+            Console.WriteLine("{0} Counter={1}", ping, await ping.GetCounter());
+            Console.WriteLine("{0} Counter={1}", pong, await pong.GetCounter());
+        }
+    }
+}
